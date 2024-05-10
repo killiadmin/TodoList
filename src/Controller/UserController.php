21e6849f630d92cd64afd21305eae07c78e3d28a
killiadmin <k.filatre@gmail.com>
2024-05-10 10:41:25 +0200

    add routes user list

---
+++ src/Controller/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/users', name: 'user_list')]
+    public function listAction(UserRepository $userRepository): Response
+    {
+        return $this->render('user/list.html.twig', ['users' => $userRepository->findAll()]);
+    }
+}
