3721e407a9a1eddb139fdf6c1681b3823408efca
killiadmin <k.filatre@gmail.com>
2024-05-17 22:02:05 +0200

    create new tasks

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,20 +2,68 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskFormType;
 use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TaskController extends AbstractController
 {
+    /**
+     * List all tasks
+     *
+     * @param TaskRepository $taskRepository The TaskRepository instance
+     *
+     * @return Response The response object
+     */
     #[Route('/tasks', name: 'task_list')]
-    public function listAction(TaskRepository $taskRepository): Response
+    public function listAction(
+        TaskRepository $taskRepository
+    ): Response
     {
         $tasks = $taskRepository->findAll();
 
         return $this->render('task/list.html.twig', [
             'tasks' => $tasks
         ]);
+    }
+
+    /**
+     * Create a new task
+     *
+     * @param Request $request The request object
+     * @param EntityManagerInterface $em The EntityManagerInterface instance
+     *
+     * @return RedirectResponse|Response The redirect response or response object
+     */
+    #[Route('/tasks/create', name: 'task_create')]
+    public function createAction(
+        Request                $request,
+        EntityManagerInterface $em
+    ): RedirectResponse|Response
+    {
+        $task = new Task();
+        $user = $this->getUser();
+        $form = $this->createForm(TaskFormType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task->setDone(0);
+            $task->setCreatedAt(new \DateTimeImmutable());
+            $task->setIdUser($user);
+
+            $em->persist($task);
+            $em->flush();
+
+            $this->addFlash('success', 'La tâche a été bien été ajoutée.');
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->render('task/create.html.twig', ['form' => $form->createView()]);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,7 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class UserController extends AbstractController
@@ -37,15 +36,13 @@ class UserController extends AbstractController
      * @param User $user The user to edit
      * @param Request $request The HTTP request object
      * @param EntityManagerInterface $em The entity manager
-     * @param UserPasswordHasherInterface $userPasswordHasher The user password hasher
      * @return RedirectResponse|Response
      */
     #[Route('/users/{id}/edit', name: 'user_edit')]
     public function editAction(
         User                        $user,
         Request                     $request,
-        EntityManagerInterface      $em,
-        UserPasswordHasherInterface $userPasswordHasher
+        EntityManagerInterface      $em
     ): RedirectResponse|Response
     {
         $form = $this->createForm(RegistrationFormType::class, $user, ['add_password_fields' => false]);
@@ -53,12 +50,10 @@ class UserController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
             $em->persist($user);
             $em->flush();
 
-            $this->addFlash('success', "The user has been modified");
-
+            $this->addFlash('success', 'The user has been modified');
             return $this->redirectToRoute('user_list');
         }
 
